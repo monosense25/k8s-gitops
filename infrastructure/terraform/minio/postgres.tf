aca496cb854fbcd0cc474d9eab85205250d99191
monosense25 <eko.purwanto@monosense.io>
2023-11-09 17:42:30 +0700

    fix(k8tz): change timezone

--- infrastructure/terraform/minio/postgres.tf
+++ infrastructure/terraform/minio/postgres.tf
@@ -1,6 +1,6 @@
 module "s3_cloudnative-pg" {
   source      = "./modules/minio"
-  vault       = "Homelab"
+  vault       = "Automation"
   bucket_name = "cloudnative-pg"
   # The OP provider converts the fields with toLower!
   user_secret_item = "postgres_s3_secret_key"
